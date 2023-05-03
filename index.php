127fdbb7d787dfee8353742fd78be61924d4da97
MiguelFloresVillarroel420 <116700329+destroyer420vivalamariguana@users.noreply.github.com>
2023-05-03 16:29:01 -0500

    Todo con php

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php include 'page/header.php'?>
+
+<html>
+  <body>
+    <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <!-- inicio alerta -->
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'falta'){
+            ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Rellena todos los campos.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?>
+
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'registrado'){
+            ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Registrado!</strong> Se agregaron los datos.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?>   
+            
+            
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'error'){
+            ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Vuelve a intentar.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?>   
+
+
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'editado'){
+            ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Cambiado!</strong> Los datos fueron actualizados.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?> 
+
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'eliminado'){
+            ?>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Eliminado!</strong> Los datos fueron borrados.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?> 
+
+            <!-- fin alerta -->
+            <div class="card">
+                <div class="card-header">
+                    Lista de Productos
+                </div>
+                <div class="p-4">
+                    <table class="table align-middle">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nombre Producto</th>
+                                <th scope="col">Precio Producto</th>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Descripción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            <?php 
+                                foreach($persona as $dato){ 
+                            ?>
+
+                            <tr>
+                                <td scope="row"><?php echo $dato->id; ?></td>
+                                <td><?php echo $dato->nombres; ?></td>
+                                <td><?php echo $dato->apellido_paterno; ?></td>
+                                <td><?php echo $dato->apellido_materno; ?></td>
+                                <td><?php echo $dato->fecha_nacimiento; ?></td>
+                                <td><?php echo $dato->celular; ?></td>
+                                <td><a class="text-success" href="editar.php?codigo=<?php echo $dato->id; ?>"><i class="bi bi-pencil-square"></i></a></td>
+                                <td><a onclick="return confirm('Estas seguro de eliminar?');" class="text-danger" href="eliminar.php?codigo=<?php echo $dato->id; ?>"><i class="bi bi-trash"></i></a></td>
+                            </tr>
+
+                            <?php 
+                                }
+                            ?>
+
+                        </tbody>
+                    </table>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Registra el producto:
+                </div>
+                <form class="p-4" method="POST" action="registrar.php">
+                    <div class="mb-3">
+                        <label class="form-label">Nombre del Producto: </label>
+                        <input type="text" class="form-control" name="txtNombres" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Precio del Producto: </label>
+                        <input type="text" class="form-control" name="txtApPaterno" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Cantidad: </label>
+                        <input type="text"" class="form-control" name="txtApMaterno" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Descripción: </label>
+                        <input type="number" class="form-control" name="txtCelular" autofocus required>
+                    </div>
+                    <div class="d-grid">
+                        <input type="hidden" name="oculto" value="1">
+                        <input type="submit" class="btn btn-primary" value="Registrar Producto">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'page/footer.php'?>
---
+++ page/footer.php
@@ -0,0 +1,14 @@
+        <div class="b-example-divider"></div>
+
+            <div class="container"> 
+                <footer class="py-3 my-4">
+                    <p class="text-center text-muted">2023, TECSUP 3er Ciclo</p>
+                </footer>
+            </div>
+        </div>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="/docs/5.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
